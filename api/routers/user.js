e035392687a3940fc81afd9be5e8011d1f4496fc
nine96as <ashrafsaid79@gmail.com>
2023-11-23 10:47:09 +0000

    Complete user router logic

--- api/routers/user.js
+++ api/routers/user.js
@@ -1,5 +1,10 @@
 const { Router } = require('express');
+const { register, login, logout } = require('../controllers/user');
 
 const userRouter = Router();
+
+userRouter.post('/register', register);
+userRouter.post('/login', login);
+userRouter.get('/logout', logout);
 
 module.exports = userRouter;
