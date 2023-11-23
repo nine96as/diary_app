690758f1c34d2ac83c852b5e4246ccef07924e94
nine96as <nine96@skiff.com>
2023-11-23 10:50:03 +0000

    Complete entry router logic

--- api/routers/entry.js
+++ api/routers/entry.js
@@ -1,5 +1,11 @@
 const { Router } = require('express');
+const { index, create, show, destroy } = require('../controllers/entry');
 
 const postRouter = Router();
+
+postRouter.get('/', index);
+postRouter.get('/:id', show);
+postRouter.post('/', create);
+postRouter.delete('/:id', destroy);
 
 module.exports = postRouter;
