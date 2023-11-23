2cba761745435e645504787b773c58d648be616a
nine96as <nine96@skiff.com>
2023-11-23 16:33:01 +0000

    Simplify delete btn icon markup

--- client/assets/board.js
+++ client/assets/board.js
@@ -16,7 +16,7 @@ const createPostElement = (data) => {
   const deleteBtn = document.createElement('button');
   deleteBtn.id = 'delete';
   deleteBtn.innerHTML =
-    '<svg data-testid="geist-icon" fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24" style="color:var(--geist-foreground);width:24px;height:24px"><path d="M3 6h18"/><path d="M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2"/><path d="M10 11v6"/><path d="M14 11v6"/></svg>';
+    '<svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24" style="color:var(--geist-foreground);width:24px;height:24px"><path d="M3 6h18"/><path d="M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2"/><path d="M10 11v6"/><path d="M14 11v6"/></svg>';
 
   deleteBtn.addEventListener('click', async () => {
     const options = {
