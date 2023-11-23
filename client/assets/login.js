7251b92a5e37a5d295dd9b361e0f0a8aa371419f
nine96as <ashrafsaid79@gmail.com>
2023-11-23 12:22:09 +0000

    Add strict equality

--- client/assets/login.js
+++ client/assets/login.js
@@ -18,7 +18,7 @@ document.getElementById('login-form').addEventListener('submit', async (e) => {
   const response = await fetch('http://localhost:3000/users/login', options);
   const data = await response.json();
 
-  if (response.status == 200) {
+  if (response.status === 200) {
     //if token is provided we send it to local storage
     localStorage.setItem('token', data.token); // we compare if token exist in our browser localStorage -its a web api
     window.location.assign('board.html'); //redirect to post page
