e8db90023bc04a6ba65e73da0259b32d17362809
nine96as <ashrafsaid79@gmail.com>
2023-11-23 09:52:12 +0000

    Complete (actual) index file logic

--- api/index.js
+++ api/index.js
@@ -1,20 +1,5 @@
-const express = require('express');
-const cors = require('cors');
-const logger = require('morgan');
+const api = require('./api');
 
-const api = express();
-
-// middlewares
-api.use(cors());
-api.use(express.json());
-api.use(logger('dev'));
-
-api.get('/', (req, res) => {
-  res.status(200).json({
-    name: 'Diary API',
-    description: 'Create vivid snapshots of your memories.',
-    endpoints: ['']
-  });
-});
-
-module.exports = api;
+api.listen(3000, () =>
+  console.log(`API listening on port: http://localhost:${process.env.PORT}`)
+);
