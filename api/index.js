da2c4b49c610edb0997deab0414685365d6268be
nine96as <ashrafsaid79@gmail.com>
2023-11-23 09:53:10 +0000

    Tweak index clg output

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const api = require('./api');
 
 api.listen(3000, () =>
-  console.log(`API listening on port: http://localhost:${process.env.PORT}`)
+  console.log(`API listening on port: http://localhost:${3000}`)
 );
